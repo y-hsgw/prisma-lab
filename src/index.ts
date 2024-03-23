d22abc7c1b0cda32eb2775108e08133dff38c560
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-03-23 16:01:46 +0900

    usersの取得

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
