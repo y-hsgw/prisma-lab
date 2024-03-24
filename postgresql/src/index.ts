8afe360471b0948a3154a3edf8449e6bd990309c
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-03-24 15:23:40 +0900

    postgresqlにデータを書き込む

--- postgresql/src/index.ts
+++ postgresql/src/index.ts
@@ -3,35 +3,19 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.post.update({
-    where: {
-      slug: "my-first-post",
-    },
-    data: {
-      comments: {
-        createMany: {
-          data: [
-            { comment: "Great post!" },
-            { comment: "Can't wait to read more!" },
-          ],
-        },
-      },
-    },
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true },
   });
-  const posts = await prisma.post.findMany({
-    include: {
-      comments: true,
-    },
-  });
-
-  console.dir(posts, { depth: Infinity });
+  console.log(post);
 }
 
 main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
   .catch(async (e) => {
     console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
     await prisma.$disconnect();
+    process.exit(1);
   });
