bfbae3f56cfa3ff001543ab495bc6fb5b06d58e9
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-03-24 14:59:31 +0900

    postgresqlディレクトリの作成

---
+++ postgresql/src/index.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.post.update({
+    where: {
+      slug: "my-first-post",
+    },
+    data: {
+      comments: {
+        createMany: {
+          data: [
+            { comment: "Great post!" },
+            { comment: "Can't wait to read more!" },
+          ],
+        },
+      },
+    },
+  });
+  const posts = await prisma.post.findMany({
+    include: {
+      comments: true,
+    },
+  });
+
+  console.dir(posts, { depth: Infinity });
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
